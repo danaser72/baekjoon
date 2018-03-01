71abd4321eeccc3f1acd99800ad86eb9448e3ab7
danaser72 <worl5604@naver.com>
2018-03-01 22:53:00 +0900

    1753 END - Dijkstra Algorithm

--- baekjoon/1753/1753.cpp
+++ baekjoon/1753/1753.cpp
@@ -3,45 +3,79 @@
 #include <queue>
 using namespace std;
 
-vector<pair<int, int>> vec[20001];
+#define MAX 3000000
+
+struct node {
+	int endPoint;
+	int dist;
+};
+
+struct cmp {
+	bool operator () (node x, node y) {
+		return x.dist > y.dist;
+	}
+};
+
+
+void DA(int K);
+
+vector<pair <int, int>> adj[20001];
 int V, E;
 int K;
 int u, v, w;
-int d[20001] = { 0, };
-
-void DA(int st, int en);
 
 int main() {
-	cin >> V >> E;
-	cin >> K;
+	cin >> V >> E >> K;
 
 	for (int i = 0; i < E; i++) {
 		cin >> u >> v >> w;
-		
-		vec[u].push_back({ v,w });
+
+		adj[u].push_back({ v, w });
 	}
-	for (int k = 0; k < V; k++) {
-	}
+
+	DA(K);
 
 	return 0;
 }
 
-void DA(int st, int en) {
-	vector <int> Q;
-
+void DA(int K) {
+	int d[20001];
+	priority_queue <node, vector<node>, cmp> pq;
+	
+	pq.push({ K, 0 });
 	for (int i = 0; i < V; i++) {
-		Q.push_back(i + 1);
+		d[i + 1] = MAX;
+		if (i + 1 != K)
+			pq.push({ i + 1, MAX });
 	}
 
-	while (!Q.empty()) {
-		if (Q.size() == V) {
+	d[K] = 0;
+
+	while (!pq.empty()) {
+		int _end = pq.top().endPoint;
+		int _dist = pq.top().dist;
+		pq.pop();
+
+		if (_dist > d[_end]) {
+			continue;
 		}
-		else {
-			int t = Q.front();
 
-			for (int i = 0; i < V; i++) {
+		for (int i = 0; i < adj[_end].size(); i++) {
+			int dest = adj[_end][i].first;		// 목적 Point
+			int dis = adj[_end][i].second;		// _end -> i 까지의 거리
 
+			if (d[dest] > d[_end] + dis) {
+				d[dest] = d[_end] + dis;
+				pq.push({dest, d[dest]});
 			}
 		}
+	}
+
+	for (int i = 0; i < V; i++) {
+		if (d[i + 1] == MAX)
+			cout << "INF" << endl;
+		else {
+			cout << d[i + 1] << endl;
+		} 
 	}
 }
