40141b1c0e91ecbce398efb6f0f1eb673b054727
danaser72 <worl5604@naver.com>
2018-03-18 14:18:04 +0900

    1937 Solving - Use memorization

--- baekjoon/1937/1937.cpp
+++ baekjoon/1937/1937.cpp
@@ -3,18 +3,26 @@
 #include <queue>
 using namespace std;
 
+// Memorization
+
 int n;
-vector <pair<int, int>> v[502];
+vector <vector< pair<int, int>>> v;
+int dx[] = { -1,0,1,0 };
+int dy[] = { 0,-1,0,1 };
 
 int func(int x, int y);
 
 int main() {
 	cin >> n;
 
-	for (int i = 0; i < n*n; i++) {
-		int t;
-		cin >> t;
-		v[i / n].push_back({ t, 0 });
+	for (int i = 0; i < n; i++) {
+		vector <pair<int, int>> tmp;
+		for (int j = 0; j < n; j++) {
+			int t;
+			cin >> t;
+			tmp.push_back({ t,0 });
+		}
+		v.push_back(tmp);
 	}
 
 	for (int i = 0; i < n; i++) {
@@ -32,6 +40,13 @@ int main() {
 			}
 		}
 	}
+	/*
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << v[i][j].second << " ";
+		}
+		cout << endl;
+	}*/
 
 	cout << MAX << endl;
 
@@ -39,7 +54,37 @@ int main() {
 }
 
 int func(int x, int y) {
+	queue<pair<pair<int, int>, int>> q;
+	q.push({ { x, y }, 1 });
+
+	int MAX = 0;
+
+	while (!q.empty()) {
+		int _x = q.front().first.first, _y = q.front().first.second, val = q.front().second;
+		q.pop();
+
+		for (int i = 0; i < 4; i++) {
+			if (_x == -1)
+				continue;
+			else if (_x + dx[i] >= 0 && _x + dx[i] < n && _y + dy[i] >= 0 && _y + dy[i] < n) {
+				if (v[_x][_y].first < v[_x + dx[i]][_y + dy[i]].first) {
+					if (v[_x + dx[i]][_y + dy[i]].second != 0) {
+						q.push({ { -1, -1 }, val + v[_x + dx[i]][_y + dy[i]].second });
+					}
+					else {
+						q.push({ { _x + dx[i], _y + dy[i] }, val + 1 });
+					}
+				}
+			}
+		}
+		if (MAX < val)
+			MAX = val;
+	}
+
+	return MAX;
+	/*
 	int check = 0;
+
 	if (v[x][y].second != 0)
 		return v[x][y].second;
 
@@ -69,5 +114,7 @@ int func(int x, int y) {
 
 	a = a > b ? a : b;
 	c = c > d ? c : d;
+
 	return a > c ? a + 1 : c + 1;
+	*/
 }
