ad8d59ab31e1f01791daf7e0be06882ab4055937
민재기 <worl5604@naver.com>
2018-05-29 20:55:38 +0900

    180529 - 1648 : study dp

--- baekjoon/1648/1648.cpp
+++ baekjoon/1648/1648.cpp
@@ -2,12 +2,70 @@
 
 
 #include <iostream>
+#include <vector>
+#include <string.h>
 using namespace std;
 
 int N, M;
 
-int main() {
+int dp[15][15][1 << 14] = { 0, };
+int func(int x, int y, int status);
 
+int main() {
+	//cin >> N >> M;
+	memset(dp, -1, sizeof(dp));
+	scanf("%d%d", &N, &M);
+
+	if (N % 2 == 1 && M % 2 == 1) {
+		printf("0\n");
+	}
+	else {
+		//cout << func(0, 0, 0) << endl;
+		printf("%d\n", func(0, 0, 0));
+	}
 
 	return 0;
+}
+
+int func(int x, int y, int state) {
+	if (x == N) {
+		return state == 0;
+	}
+	if (y == M) {
+		return func(x + 1, 0, state);
+	}
+	if (y > M)
+		return 0;
+
+	if (dp[x][y][state] != -1)
+		return dp[x][y][state];
+
+	int ret = 0;
+
+	if (state & 1) {
+		int st = state;
+		st = st >> 1;
+		//st = st | 0;
+		//st = st & ((1 - M) - 1);
+
+		ret += func(x, y + 1, st);
+	}
+	else {
+		int st = state;
+		st = st >> 1;
+		st = st | (1 << (M - 1));
+		//st = st & ((1 - M) - 1);
+
+		ret += func(x, y + 1, st);
+
+		if (!(state & (1 << 1))) {
+			st = state;
+			st = st >> 2;
+			ret += func(x, y + 2, st);
+		}
+	}
+	ret %= 9901;
+	dp[x][y][state] = ret;
+
+	return ret;
 }
--- baekjoon/2718/2718.cpp
+++ baekjoon/2718/2718.cpp
@@ -3,6 +3,66 @@
 				http://joonas-yoon.blogspot.kr/2016/03/2718.html
 */
 
+#define bitmask
+
+
+#ifdef bitmask
+
+#include <iostream>
+using namespace std;
+
+int T;
+int N;
+int dp[1002][5];
+
+void func();
+
+int main() {
+	for (int i = 0; i < 1002; i++) {
+		for (int j = 0; j < 5; j++) {
+			dp[i][j] = -1;
+		}
+	}
+
+	cin >> T;
+
+	while (T--) {
+		cin >> N;
+
+		func();
+
+		cout << dp[N][0] << endl;
+	}
+
+	return 0;
+}
+
+void func() {
+	dp[0][0] = 1;
+	dp[0][1] = 0;
+	dp[0][2] = 0;
+	dp[0][3] = 0;
+	dp[0][4] = 0;
+
+	for (int i = 1; i <= N; i++) {
+		if (i == 1) {
+			dp[i][0] = 1;
+		}
+		else {
+			dp[i][0] = dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][3] + dp[i - 1][4] + dp[i - 2][0];
+		}
+		dp[i][1] = dp[i - 1][0] + dp[i - 1][4];
+		dp[i][2] = dp[i - 1][3];
+		dp[i][3] = dp[i - 1][0] + dp[i - 1][2];
+		dp[i][4] = dp[i - 1][0] + dp[i - 1][1];
+	}
+}
+
+#endif
+
+#ifdef DP
+
+
 #include <iostream>
 using namespace std;
 
@@ -34,3 +94,5 @@ int main() {
 
 	return 0;
 }
+
+#endif
