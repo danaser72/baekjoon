d0592021fda346524147528786edc72f7bdbd2c3
민재기 <worl5604@naver.com>
2018-06-26 23:25:45 +0900

    180626 - 9466

--- baekjoon/9466/9466.cpp
+++ baekjoon/9466/9466.cpp
@@ -1,3 +1,7 @@
+/*
+**	ref : http://js1jj2sk3.tistory.com/40
+*/
+
 #include <iostream>
 #include <string.h>
 #include <vector>
@@ -5,30 +9,33 @@ using namespace std;
 
 int T, N;
 
-int choice[100002];
+int arr[100002];
 int cycle[100002];
 int visit[100002];
 int cnt;
 
-
-void func(int t, int rot, vector<int> v);
+int func(int start, int cur, int cnt);
 
 int main() {
 	scanf("%d", &T);
 
 	while (T--) {
-		memset(cycle, 0, sizeof(choice));
-		memset(visit, 0, sizeof(choice));
 		cnt = 0;
 
 		scanf("%d", &N);
 
-		for (int i = 1; i <= N; i++)
-			scanf("%d", &choice[i]);
+		for (int i = 1; i <= N; i++) {
+			scanf("%d", &arr[i]);
+			cycle[i] = 0;
+			visit[i] = 0;
+		}
 
 		for (int i = 1; i <= N; i++) {
-			vector<int> v;
-			func(i, 1, v);
+			if (visit[i] != 0)
+				continue;
+			if (visit[arr[i]] == 0) {
+				cnt += func(i, i, 1);
+			}
 		}
 
 		printf("%d\n", N - cnt);
@@ -37,15 +44,19 @@ int main() {
 	return 0;
 }
 
-void func(int t, int rot, vector<int> v) {
-	if (visit[t]) {
-		if (cycle[t] == 0) {
-			cnt = cnt + rot - visit[t];
+int func(int start, int cur, int count) {	
+	visit[cur] = count;
+	cycle[cur] = start;
+
+	if (visit[arr[cur]]) {
+		if (start == cycle[arr[cur]]) {
+			return count - visit[arr[cur]] + 1;
+		}
+		else {
+			return 0;
 		}
 	}
 	else {
-		visit[t] = rot;
-		rot++;
-		func(choice[t], rot);
+		return func(start, arr[cur], count + 1);
 	}
 }
